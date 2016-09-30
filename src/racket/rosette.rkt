2f60d7510fefd350d1bf47b494b1188bdcd0ad37
Konstantin Weitz <weitzkon@uw.edu>
2016-09-29 19:18:24 -0700

    remove unused code

--- src/racket/rosette.rkt
+++ src/racket/rosette.rkt
@@ -21,28 +21,9 @@
             (values c (solvable-default (get-type c))))))]
        [_ (error 'complete "expected a sat? solution, given ~a" sol)])]))
 
-(define (concretize-solution m consts)
-  (sat (for/hash ([c consts])
-         (match (m c)
-           [(constant _ (== integer?))
-            (values c 0)]
-           [(constant _ (== boolean?))
-            (values c #f)]
-       ;   [(constant _ (? enum? t))
-       ;    (values c (enum-first t))]
-           [val (values c val)]))))
-
 (define-syntax-rule (solve/evaluate/concretize expr)
   (let* ([out (void)]
          [sol (solve (set! out (expr (void))))])
     (if (unsat? sol) '(Nil)
       `(Cons ,(evaluate out (concretize sol)) (Nil)))))
-
-; (let ([v (solve (expr void))])
-;   (if (unsat? v) '(None)
-;     `(Some ,(void)))))
-
-;   (let ([v (solve/evaluate (expr void))])
-;     `(Some ,(evaluate v (concretize-solution (current-solution) (symbolics v)))))
-
 

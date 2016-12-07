513f6647a5e2f9eadd123369d7f414cb7e54a1e8
Konstantin Weitz <weitzkon@uw.edu>
2016-12-06 20:13:55 -0800

    add Cpdt Tactics

--- src/coq/EqDec.v
+++ src/coq/EqDec.v
@@ -14,6 +14,7 @@ Require Import Coq.Arith.EqNat.
 Require Import Coq.Bool.Sumbool.
 Import EqNotations.
 Require Import Arith.
+Require Import CpdtTactics.
 
 Class eqDec A := {
   eqDecide : forall (a a':A), {a=a'} + {a<>a'}
@@ -27,4 +28,21 @@ Defined.
 
 Global Instance eqDecProd {A B} `{eqDec A} `{eqDec B} : eqDec (A * B).
   constructor; repeat decide equality; apply eqDecide.
+Defined.
+
+Instance eqDecSigT {A B} `{eqDec A} `{forall a:A, eqDec (B a)} : eqDec {a:A & B a}.
+  constructor. intros x x'. 
+  destruct x.
+  destruct x'.
+  match goal with a:A, a':A |- _ => destruct (a =? a') end.
+  - subst. 
+    match goal with a:A, b:B ?a, b':B ?a |- _ => destruct (b =? b') end.
+    + left.
+      subst.
+      reflexivity.
+    + right. 
+      crush.
+  - right. 
+    injection.
+    congruence.
 Defined.
